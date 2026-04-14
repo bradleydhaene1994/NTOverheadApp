f963e177246198b7a78c7d2c70171a3ef953f56d
bradleydhaene1994 <204046398+bradleydhaene1994@users.noreply.github.com>
2026-04-14 13:18:27 +0200

    basic setup backend

---
+++ Backend/NTOverheadApp/Language.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public enum Language
+{
+    nl,
+    fr,
+    en,
+    de
+}
---
+++ Backend/NTOverheadApp/Patient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Patient
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int LastPlayedLevel { get; set; }
+    public Language Laguage { get; set; }
+    public DateTime? LastPlayed {  get; set; }
+}
